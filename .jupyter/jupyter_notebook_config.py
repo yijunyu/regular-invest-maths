c54b68d86edae8330f6a5e8b4727f0d7b2f62ee5
Yijun Yu <y.yu@open.ac.uk>
2019-12-29 18:09:36 +0000

    update

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,4 +1,4 @@
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.allow_origin = '*'
-c.NotebookApp.password = u'sha1:649b9c4f6931a6d274d0309eddb6da50fbcc92c9'
 c.NotebookApp.open_browser = True
+c.NotebookApp.password = u'sha1:047162fc0148:11d91842f560f5a3e6a74c28ea56d4032ee1ebc6'
